b557d517c7d8fa954580f742100476ca4f1e0fd3
Henry Ching <hching@yahoo-inc.com>
2014-01-10 20:56:02 -0800

    Initial Commit

---
+++ temperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  temperatureConverter
+//
+//  Created by Henry Ching on 1/10/14.
+//  Copyright (c) 2014 YahooHenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
